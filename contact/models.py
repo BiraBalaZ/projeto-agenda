2d5f6298e44e66fa83d344f65c5212c481369e7a
BiraBalaZ <erick.m.anjos@gmail.com>
2025-05-12 22:40:00 -0300

    Creating my First Model and Migrating

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# ID (primary key - automatico)
+# first_name (string), last_name (string), phone (string),
+# email (email), created_date (date), description (text),
+# category (foreign key), show (boolean), owner (foreign key),
+# picture (image).
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 20, blank = True)
+    phone = models.CharField(max_length = 15)
+    email = models.EmailField(max_length = 254, blank = True)
+    created_date = models.DateTimeField(default = timezone.now)
+    description = models.TextField(blank = True)
+    # category = models.ForeignKey("app.Model", on_delete = models.CASCADE)
+    # show = models.BooleanField()
+    # owner = models.ForeignKey("app.Model", on_delete = models.CASCADE)
+    # picture = models.ImageField(upload_to = None, height_field = None, width_field = None, max_length = None)
+
+
+
